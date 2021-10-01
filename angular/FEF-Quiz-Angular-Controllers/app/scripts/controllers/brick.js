c2675f34ad24c32b41e15cbf5f08d16fc7c9b6cd
hchiam <hchiam@users.noreply.github.com>
2021-09-30 22:34:37 -0400

    got the controller and view/template working

---
+++ angular/FEF-Quiz-Angular-Controllers/app/scripts/controllers/brick.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * @ngdoc function
+ * @name controllerQuizApp.controller:BrickCtrl
+ * @description
+ * # BrickCtrl
+ * Controller of the controllerQuizApp
+ */
+angular.module("controllerQuizApp").controller("BrickCtrl", function () {
+  this.color = "color";
+  this.size = "size";
+  this.price = "price";
+  this.quantity = "quantity";
+});
