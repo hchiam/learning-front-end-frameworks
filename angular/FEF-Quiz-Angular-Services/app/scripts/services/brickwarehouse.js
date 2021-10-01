56a669ef83053ae09c78f694a1cafcc0d4522cda
hchiam <hchiam@users.noreply.github.com>
2021-09-30 23:14:06 -0400

    used yeoman to generate service

---
+++ angular/FEF-Quiz-Angular-Services/app/scripts/services/brickwarehouse.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name serviceQuizApp.brickWarehouse
+ * @description
+ * # brickWarehouse
+ * Service in the serviceQuizApp.
+ */
+angular.module('serviceQuizApp')
+  .service('brickWarehouse', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
