9a5baed69ddfc63e834a9651f374ba228343d4f3
hchiam <hchiam@users.noreply.github.com>
2021-09-30 22:14:38 -0400

    remove main.js and use yeoman to generate menu.js controller files

---
+++ angular/FEF-Quiz-Angular-Up-and-Running/app/scripts/controllers/menu.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name q1UpAndRunningApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the q1UpAndRunningApp
+ */
+angular.module('q1UpAndRunningApp')
+  .controller('MenuCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
